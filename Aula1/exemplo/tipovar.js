53fcc707ffd4759ac8526174b573845e90541d3e
Filcst11 <filcosta11@gmail.com>
2024-03-07 13:27:00 -0300

    Exemplos em sala realizados com auxílio do prof.

---
+++ Aula1/exemplo/tipovar.js
@@ -0,0 +1,17 @@
+const n = 3
+//nn pode alterar nem redeclarar uma variável com const
+//const n = 54
+
+
+var f = "ola mundo"
+var f = "55"
+f = 43
+//Pode alterar o valor e ser redeclarada
+
+let g = 99
+//let g = 65 Nn pode 
+g = 65 
+//pode alterar o valor
+//let é o mais recomendável
+
+console.log('const n =',n,'\nvar f =',f,'\nlet g =',g)
