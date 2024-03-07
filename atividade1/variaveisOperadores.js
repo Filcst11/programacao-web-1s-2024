e95e953598acecc27bc3cab9947d62f5d1a34252
Filcst11 <filcosta11@gmail.com>
2024-03-07 13:48:47 -0300

    Atividade 1 proposta pelo professor em sala

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,28 @@
+//1- Faça uma declaração de 2 variáveis numéricas para cada operador e guarde o resultado da operação em uma terceira variável 
+
+//Soma
+let so1 = 5
+let so2 = 6
+let soma = so1 + so2
+
+//Subtração
+let su1 = 28
+let su2 = 10
+let subtracao = su1 - su2
+
+//Multiplicação
+let m1 = 12
+let m2 = 11
+let multiplicacao = m1 * m2
+
+//Divisão
+let d1 = 800
+let d2 = 5
+let divisao = d1 / d2
+
+//2- Para cada resultado obtido, imprima o texto "Resultado da {operação} entre {num_A} e {num_B} é {resultado}"
+
+console.log('\nResultado da Soma entre',so1,'e',so2,'é',soma)
+console.log('\nResultado da Subtração entre',su1,'e',su2,'é',subtracao)
+console.log('\nResultado da Multiplicação entre',m1,'e',m2,'é',multiplicacao)
+console.log('\nResultado da Divisão entre',d1,'e',d2,'é',divisao,'\n')
