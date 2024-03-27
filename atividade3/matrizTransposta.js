3b4a70fc8c43f4fd978856e31aa7cab1bb38de53
Filcst11 <filcosta11@gmail.com>
2024-03-27 15:17:53 -0300

    Atividade 3 versão 2.0.0

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -1,3 +1,41 @@
+const A =[  
+    [1,2],
+    [3,4],
+    [5,6]
+] 
+
 function transporMatriz(A){
+    let i = 0
+    //Imprime matriz
+    console.log("Matriz:\n\n")
+    while(A[i] !== undefined){
+        console.log(A[i])
+        i+=1
+    }
+    
+    //Cria matriz transposta
+    i = 0  
+    let B = [[],[]] 
+    //Anda entre as linhas
+    while(A[i] !== undefined){
+        let j = 0
+        
+        //Anda entre as colunas
+        while(A[i][j] !== undefined){
+            B[j][i] = A[i][j]
+            j+=1
+        }  
+        
+        i+=1
+    }  
+    //imprime matriz transposta  
+    console.log("\n\nMatriz Transposta:\n")
+    i=0
+    while(B[i] !== undefined){
+        console.log(B[i])
+        i+=1
+    }
     
 }
+
+transporMatriz(A)
