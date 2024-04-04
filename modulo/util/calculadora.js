753b27d1fb23ea862d16c869b1a88a00db77109c
Filcst11 <filcosta11@gmail.com>
2024-04-04 10:31:41 -0300

    Modulos feitos em sala de aula

---
+++ modulo/util/calculadora.js
@@ -0,0 +1,15 @@
+function calculaSoma(x,y){
+    return x + y;
+}
+
+function calculaSubtracao(x,y){
+    return x - y;
+}
+
+const Pi = 3.14
+
+module.exports = {
+    calculaSoma,
+    calculaSubtracao,
+    Pi
+}
