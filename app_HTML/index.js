d7d96ca7b0dfeb7cf2279382dc91e0eaa5707a38
Filcst11 <filcosta11@gmail.com>
2024-04-25 11:52:36 -0300

    Implementa pagina web com integração front-end

---
+++ app_HTML/index.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const app = express()
+const mustacheExpress = require('mustache-express')
+
+app.engine('html',mustacheExpress())
+app.set('view engine','html')
+app.set('views',__dirname + '/views')
+
+app.get('/',function(req,res){
+    let nome = "Felipe"
+    
+    res.render('index.html', {nome})
+})
+
+app.get('/contato', function(req, res){
+    let telefone = '(61)99291-6996'
+    let email = 'filcosta11@gmail.com'
+    
+   
+    res.render('contato.html', {telefone, email})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 8080
+app.listen(PORT,function(){
+    console.log("App rodando na porta "+PORT)
+})
